d2850185380b0c5b01189e0896e0c73a4b0febe8
craftinators <craftinatorssx@gmail.com>
2026-03-01 03:05:26 -0500

    feat: add `CodeMirror` editor component

---
+++ src/lib/components/Editor.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import { onMount, onDestroy } from 'svelte';
+    import { EditorView, keymap, lineNumbers, highlightActiveLine } from '@codemirror/view';
+    import { EditorState } from '@codemirror/state';
+    import { defaultKeymap, historyKeymap, history } from '@codemirror/commands';
+    import { oneDark } from '@codemirror/theme-one-dark';
+
+    export let value = '';
+
+    let container: HTMLDivElement;
+    let view: EditorView;
+
+    onMount(() => {
+        const state = EditorState.create({
+            doc: value,
+            extensions: [
+                history(),
+                lineNumbers(),
+                highlightActiveLine(),
+                keymap.of([...defaultKeymap, ...historyKeymap]),
+                oneDark,
+                EditorView.updateListener.of((update) => {
+                    if (update.docChanged) {
+                        value = update.state.doc.toString();
+                    }
+                }),
+            ],
+        });
+
+        view = new EditorView({ state, parent: container });
+    });
+
+    onDestroy(() => view?.destroy());
+</script>
+
+<div bind:this={container} class="editor"></div>
+
+<style>
+    .editor {
+        height: 100%;
+    }
+
+    .editor :global(.cm-editor) {
+        height: 100%;
+    }
+
+    .editor :global(.cm-scroller) {
+        font-family: monospace;
+        font-size: 14px;
+        overflow: auto;
+    }
+</style>
